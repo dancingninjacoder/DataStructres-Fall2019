006b955821c343b68ade4213593747be61f55703
dancingninjacoder <52933327+dancingninjacoder@users.noreply.github.com>
2019-10-30 23:26:08 -0600

    Add files via upload

---
+++ LAB4/BTTree.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+
+class BTreeNode:
+    # Constructor
+    def __init__(self, keys=[], children=[], is_leaf=True, max_num_keys=5):
+        self.keys = keys
+        self.children = children
+        self.is_leaf = is_leaf
+        if max_num_keys < 3:  # max_num_keys must be odd and greater or equal to 3
+            max_num_keys = 3
+        if max_num_keys % 2 == 0:  # max_num_keys must be odd and greater or equal to 3
+            max_num_keys += 1
+        self.max_num_keys = max_num_keys
+
+    def is_full(self):
+        return len(self.keys) >= self.max_num_keys
+
+
+class BTree:
+    # Constructor
+    def __init__(self, max_num_keys=5):
+        self.max_num_keys = max_num_keys
+        self.root = BTreeNode(max_num_keys=max_num_keys)
+
+    def find_child(self, k, node=None): #Changed how it compares values using the key lists. 
+        # Determines value of c, such that k must be in subtree node.children[c], if k is in the BTree
+        if node is None:
+            node = self.root
+
+        k_num = 0
+        for i in range(len(node.keys)):
+            #print(node.keys[i], k)
+            key = node.keys[i]
+            #print(str(k[1]))
+            k_num = k[1] 
+            if k_num < key[1]:
+                return i
+
+        return len(node.keys)
+
+    def insert_internal(self, i, node=None):
+
+        if node is None:
+            node = self.root
+
+        # node cannot be Full
+        if node.is_leaf:
+            self.insert_leaf(i, node)
+        else:
+            k = self.find_child(i, node)
+
+            if node.children[k].is_full():
+                m, l, r = self.split(node.children[k])
+                node.keys.insert(k, m)
+                node.children[k] = l
+                node.children.insert(k + 1, r)
+                k = self.find_child(i, node)
+
+            self.insert_internal(i, node.children[k])
+
+    def split(self, node=None):
+        if node is None:
+            node = self.root
+        # print('Splitting')
+        # PrintNode(T)
+        mid = node.max_num_keys // 2
+
+        if node.is_leaf:
+            left_child = BTreeNode(node.keys[:mid], max_num_keys=node.max_num_keys)
+            right_child = BTreeNode(node.keys[mid + 1:], max_num_keys=node.max_num_keys)
+        else:
+            left_child = BTreeNode(node.keys[:mid], node.children[:mid + 1], node.is_leaf, max_num_keys=node.max_num_keys)
+            right_child = BTreeNode(node.keys[mid + 1:], node.children[mid + 1:], node.is_leaf, max_num_keys=node.max_num_keys)
+
+        return node.keys[mid], left_child, right_child
+
+    def insert_leaf(self, i, node=None):
+        if node is None:
+            node = self.root
+
+        node.keys.append(i)
+        node.keys.sort(key = lambda r:r[1])
+        #sorted(node.keys)
+
+    def leaves(self, node=None):
+        if node is None:
+            node = self.root
+        # Returns the leaves in a b-tree
+        if node.is_leaf:
+            return [node.keys]
+        s = []
+        for c in node.children:
+            s = s + self.leaves(c)
+        return s
+
+    def insert(self, i, node=None):
+        if node is None:
+            node = self.root
+        if not node.is_full():
+            self.insert_internal(i, node)
+        else:
+            m, l, r = self.split(node)
+            node.keys = [m]
+            node.children = [l, r]
+            node.is_leaf = False
+            k = self.find_child(i, node)
+            self.insert_internal(i, node.children[k])
+
+    def height(self, node=None):
+        if node is None:
+            node = self.root
+        if node.is_leaf:
+            return 0
+        return 1 + self.height(node.children[0])
+
+    def search(self, k, node=None):
+        if node is None:
+            node = self.root
+        # Returns node where k is, or None if k is not in the tree
+        for i in range(len(node.keys)): #Change the loop to handle key lists.
+            key = node.keys[i]
+            if (k[1] == key[1]):
+                return node
+        if node.is_leaf:
+            return None
+
+        return self.search(k, node.children[self.find_child(k, node)])
